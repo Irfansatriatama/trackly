0ea706c8ad77a868818be5c62176b0219cc2c20c
irfan <irfansatriatama@gmail.com>
2026-03-06 11:02:36 +0700

    update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
  * Cache-first for all static assets; network-first for external resources.
  */
 
-const CACHE_NAME = 'trackly-v1.6.1';
+const CACHE_NAME = 'trackly-v1.6.2';
 
 const STATIC_ASSETS = [
   './',
